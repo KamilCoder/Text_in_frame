d6335280e0c51514aad51c92e97e9fe65bca4e57
Kamil <59434156+KamilCoder@users.noreply.github.com>
2022-01-31 19:10:08 +0100

    Update forest.py

--- forest.py
+++ forest.py
@@ -1,39 +1,40 @@
 class Frame:
-	def __init__(self, content, borderCharacter, space = ' '):
+    def __init__(self, content, border_character, space=' '):
 		self.content = content
-		self.borderCharacter = borderCharacter
+		self.border_character = border_character
 		self.space = space
-		self.upDownLinesHeight = 2
-		self.widthSpacesAndLines = 4
-		self.totalWidth = len(max(content,key=len))+self.widthSpacesAndLines
-		self.totalHeight = len(content)+self.upDownLinesHeight
-		self.newLineForLinux='\n'
-		self.lastLine = self.totalHeight - 3
-	def getLine(self):
-		return self.borderCharacter*self.totalWidth
+		self.up_down_lines_height = 2
+		self.width_spaces_lines = 4
+		self.total_width = len(max(content, key=len)) + self.width_spaces_lines
+		self.total_height = len(content) + self.up_down_lines_height
+		self.new_line_for_linux = '\n'
+		self.last_line = self.total_height - 3
 
-	def setSpaces(self, line):
-		return self.space*(self.totalWidth -3 -(len(self.content[line])))
+    def get_line(self):
+        return self.border_character * self.total_width
 
-	def setContentLine(self, line):
-		return self.borderCharacter + self.space + self.content[line]+ self.setSpaces(line) +self.borderCharacter
+	def set_spaces(self, line):
+		return self.space*(self.total_width - 3 - (len(self.content[line])))
 
-	def getContent(self):
+	def set_content_line(self, line):
+		return self.border_character + self.space + self.content[line]
+                + self.set_spaces(line) + self.border_character
+
+	def get_content(self):
 		content = str()
-		for line in range(self.totalHeight-self.upDownLinesHeight):
-			if line == self.lastLine:
-				content+=self.setContentLine(line)
+		for line in range(self.total_height - self.up_down_lines_height):
+			if line == self.last_line:
+				content += self.set_content_line(line)
 			else:
-				content+=self.setContentLine(line)+self.newLineForLinux
+				content += self.set_content_line(line) + self.new_line_for_linux
 
 		return content
 
 	def __str__(self):
-		return self.newLineForLinux.join((self.getLine(),
-										self.getContent(),
-										self.getLine()))
+		return self.new_line_for_linux.join((self.get_line(), self.get_content(),
+                                             self.get_line()))
 
 	def __repr__(self):
-		return self.newLineForLinux.join((self.getLine(),
-										self.getContent(),
-										self.getLine()))
+		return self.new_line_for_linux.join((self.get_line(), self.get_content(),
+                                             self.get_line()))
+	
